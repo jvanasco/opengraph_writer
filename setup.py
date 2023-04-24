c54def0229a500bbff2cc6c58a484f49ca9be288
jonathan vanasco <jonathan@2xlp.com>
2023-04-24 15:42:43 -0400

    reintegrating earlier pythons to see if typing is now compatible

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         "Intended Audience :: Developers",
         "Framework :: Pyramid",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
